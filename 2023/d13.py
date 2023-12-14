321d76e576ba44e6af106124d639ba23078493b3
Daniel García Moreno <dani@danigm.net>
2023-12-14 20:58:51 +0100

    2023 day 13, part 2

--- 2023/d13.py
+++ 2023/d13.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from base import Problem
 
 
@@ -25,6 +24,37 @@ example = """
 """
 
 
+example2 = """
+.....####..
+##...####.#
+##..#....#.
+.....####..
+####.####.#
+##..#.##.#.
+##.###..###
+...#......#
+..#.######.
+##.##.##.##
+..#..#..#..
+##.#.####.#
+####..##..#
+
+#..##...#.####.##
+#..##...#.####.##
+....###.#..#.###.
+#####..####..#.##
+#..#..##...###..#
+.##.##..#..###...
+.##.#.#.#...#..#.
+##.#...#.#.....#.
+........######.#.
+....#..#....#..##
+#######..#.##..#.
+#..#...#######..#
+....###..###.##.#
+"""
+
+
 def test_part1():
     p = D13(example)
     assert len(p.patterns) == 2
@@ -48,7 +78,19 @@ def test_part1():
 
 def test_part2():
     p = D13(example)
-    assert p.solve_p2() == 0
+    p0, p1 = p.patterns
+    assert p0.fix_smudge((5, V)) == (300, (0, 0))
+    assert p1.fix_smudge((4, H)) == (100, (0, 4))
+    assert p.solve_p2() == 400
+
+
+def test_bug():
+    p = D13(example2)
+    p0, p1 = p.patterns
+    assert p0.reflection(V) == 1
+    assert p0.fix_smudge((1, V)) == (7, (1, 3))
+    assert p1.reflection(H) == 1
+    assert p1.fix_smudge((1, H)) == (2, (7, 1))
 
 
 class Pattern:
@@ -62,14 +104,20 @@ class Pattern:
             self.columns.append("".join(c))
 
     def reflection(self, direction=H):
-        n = 0
         data = self.rows
         if direction == V:
             data = self.columns
 
+        return self._ref(data)
+
+    def _ref(self, data, ignore=None):
+        n = 0
         l = len(data)
         # for each row/column, we check if it's a mirror
         for i in range(l):
+            if i + 1 == ignore:
+                continue
+
             next = range(i + 1, l)
             prev = range(i, -1, -1)
             isref = False
@@ -85,6 +133,37 @@ class Pattern:
             if isref:
                 n = i + 1
                 break
+
+        return n
+
+    def fix_smudge(self, ignore):
+        n = 0
+        data = self.rows
+        data1 = [list(i) for i in self.rows]
+        data2 = [list(i) for i in self.columns]
+        ig, dir = ignore
+
+        ni = len(data)
+        nj = len(data[0])
+        # iterate through all points and try inverting
+        for i in range(ni):
+            for j in range(nj):
+                v = data1[i][j]
+                if v == "#":
+                    data1[i][j] = "."
+                    data2[j][i] = "."
+                else:
+                    data1[i][j] = "#"
+                    data2[j][i] = "#"
+                x = self._ref(data2, ignore=ig if dir == V else None)
+                if x:
+                    return x, (i, j)
+                y = self._ref(data1, ignore=ig if dir == H else None)
+                if y:
+                    return y * 100, (i, j)
+
+                data1[i][j] = v
+                data2[j][i] = v
 
         return n
 
@@ -105,7 +184,17 @@ class D13(Problem):
         return n
 
     def solve_p2(self):
-        return 0
+        n = 0
+        for p in self.patterns:
+            n3, n4 = 0, 0
+            n1 = p.reflection(H)
+            if n1:
+                n3 = p.fix_smudge((n1, H))[0]
+            else:
+                n2 = p.reflection(V)
+                n4 = p.fix_smudge((n2, V))[0]
+            n += n3 + n4
+        return n
 
     def parseinput(self, lines):
         data = [i.strip() for i in lines]
