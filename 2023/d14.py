ccedb393551f078781ff338d12b4f56d1db6f168
Daniel García Moreno <dani@danigm.net>
2023-12-16 14:03:26 +0100

    2023 day 14 part 2

--- 2023/d14.py
+++ 2023/d14.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from base import Problem
 
 
@@ -15,6 +16,103 @@ O.#..O.#.#
 """
 
 
+ex_north = """
+OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#....
+"""
+
+
+ex_south = """
+.....#....
+....#....#
+...O.##...
+...#......
+O.O....O#O
+O.#..O.#.#
+O....#....
+OO....OO..
+#OO..###..
+#OO.O#...O
+"""
+
+
+ex_west = """
+O....#....
+OOO.#....#
+.....##...
+OO.#OO....
+OO......#.
+O.#O...#.#
+O....#OO..
+O.........
+#....###..
+#OO..#....
+"""
+
+
+ex_east = """
+....O#....
+.OOO#....#
+.....##...
+.OO#....OO
+......OO#.
+.O#...O#.#
+....O#..OO
+.........O
+#....###..
+#..OO#....
+"""
+
+
+cycle1 = """
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+"""
+
+cycle2 = """
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+"""
+
+
+cycle3 = """
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O
+"""
+
+
 North = 0
 South = 1
 West = 2
@@ -27,52 +125,153 @@ def test_part1():
 
 
 def test_part2():
+    print("Test part2")
     p = D14(example)
-    assert p.solve_p2() == 0
+    n, tilted = p.tilt(p.data, North)
+    assert n == 136
+    assert p.to_str(tilted).strip() == ex_north.strip()
+
+    n, tilted = p.tilt(p.data, South)
+    assert n == 4 + 2*2 + 3*4 + 4 + 5*2 + 6*4 + 8
+    assert p.to_str(tilted).strip() == ex_south.strip()
+
+    n, tilted = p.tilt(p.data, West)
+    assert n == 2 + 3 + 4*3 + 5*2 + 6*2 + 7*4 + 9*3 + 10
+    assert p.to_str(tilted).strip() == ex_west.strip()
+
+    n, tilted = p.tilt(p.data, East)
+    assert n == 2 + 3 + 4*3 + 5*2 + 6*2 + 7*4 + 9*3 + 10
+    assert p.to_str(tilted).strip() == ex_east.strip()
+
+    n, c1 = p.cycle(p.data)
+    assert p.to_str(c1).strip() == cycle1.strip()
+    n, c2 = p.cycle(c1)
+    assert p.to_str(c2).strip() == cycle2.strip()
+    n, c3 = p.cycle(c2)
+    assert p.to_str(c3).strip() == cycle3.strip()
+
+    assert p.solve_p2() == 64
 
 
 class D14(Problem):
     def solve_p1(self):
-        return self.tilt(North)
+        return self.tilt(self.data, North)[0]
 
     def solve_p2(self):
-        return 0
+        """
+        Iterate 1 million times and look for a cycle. Then stop and calculate
+        just the rest, if we repeat the cycle N times, we'll be in the same
+        position, so no need to calculate more than that.
+        """
 
-    def tilt(self, direction=North):
-        R = len(self.data)
-        C = len(self.data[0])
+        times = 1_000_000_000
+        d = self.data
+        repeat = 0
+        visited = {}
+        while times:
+            n, d = self.cycle(d)
+            s = self.to_str(d)
+            if s in visited:
+                # dead end
+                break
+            visited[s] = repeat
+            repeat += 1
+            times -= 1
 
-        L = C
-        if direction in [North, South]:
+        diff = times % (repeat - visited[s]) - 1
+        while diff:
+            n, d = self.cycle(d)
+            s = self.to_str(d)
+            diff -= 1
+        return n
+
+    def to_str(self, data):
+        ret = []
+
+        for row in data:
+            ret.append("".join(c for c in row))
+
+        return "\n".join(ret)
+
+    @lru_cache(maxsize=None)
+    def cycle(self, data):
+        _, t1 = self.tilt(data, North)
+        _, t2 = self.tilt(t1, West)
+        _, t3 = self.tilt(t2, South)
+        n, t4 = self.tilt(t3, East)
+
+        return n, t4
+
+    @lru_cache(maxsize=None)
+    def tilt(self, data, direction=North):
+        R = len(data)
+        C = len(data[0])
+        ndata = [list("." * C) for i in range(R)]
+
+        L = R
+        if direction == North:
             L = R
+        elif direction == South:
+            L = 1
 
         n = 0
         for i in range(R):
-            load = L
-            for j in range(C):
-                if direction == North:
-                    rock = self.data[j][i]
-                elif direction == South:
-                    rock = self.data[-(j + 1)][i]
-                elif direction == West:
-                    rock = self.data[i][j]
-                else:
-                    rock = self.data[-(i+1)][j]
+            if direction in [West, East]:
+                load = L - i
+            else:
+                load = L
+            if direction == West:
+                p = 0
+            else:
+                p = -1
 
+            for j in range(C):
+                ci, cj = i, j
+                oi, oj = i, L - load
+                if direction == North:
+                    ci, cj = j, i
+                    oi, oj = L - load, i
+                elif direction == South:
+                    ci, cj = -(j + 1), i
+                    oi, oj = -load, i
+                elif direction == West:
+                    ci, cj = i, j
+                    oi, oj = i, p
+                else:
+                    ci, cj = i, -(j+1)
+                    oi, oj = i, p
+
+                rock = data[ci][cj]
                 if rock == "#":
                     # solid rock
-                    load = L - (j + 1)
+                    if direction == South:
+                        load = L + (j + 1)
+                    elif direction == North:
+                        load = L - (j + 1)
+                    elif direction == West:
+                        p = cj + 1
+                    else:
+                        p = cj - 1
+                    ndata[ci][cj] = "#"
                     continue
+
                 if rock == "O":
                     n += load
                     # rounded rock
-                    load -= 1
-                    continue
+                    ndata[oi][oj] = "O"
+                    if direction == South:
+                        load += 1
+                    elif direction == North:
+                        load -= 1
+                    elif direction == West:
+                        p += 1
+                    else:
+                        p -= 1
 
-        return n
+        return n, tuple(tuple(i) for i in ndata)
 
     def parseinput(self, lines):
-        data = [list(i.strip()) for i in lines if i.strip()]
+        data = tuple(tuple(i.strip()) for i in lines if i.strip())
         return data
 
 
