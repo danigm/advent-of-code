3b74d8087330948dd9d245e5c90796a23eb9a2af
Daniel García Moreno <dani@danigm.net>
2023-12-16 20:58:09 +0100

    2023 day 15, part 2

--- 2023/d15.py
+++ 2023/d15.py
@@ -15,7 +15,10 @@ def test_part1():
 
 def test_part2():
     p = D15(example)
-    assert p.solve_p2() == 0
+    assert hash("rn") == 0
+    assert hash("qp") == 1
+
+    assert p.solve_p2() == 145
 
 
 def hash(line):
@@ -27,22 +30,78 @@ def hash(line):
     return v
 
 
+class Lens:
+    def __init__(self, tag, focal):
+        self.tag = tag
+        self.focal = focal
+
+    def __eq__(self, other):
+        return self.tag == other.tag
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        return f"[{self.tag} {self.focal}]"
+
+
 class D15(Problem):
     def solve_p1(self):
         return sum(hash(i) for i in self.data)
 
     def solve_p2(self):
-        return 0
+        self.hashmap()
+        n = 0
+        for i, box in enumerate(self.boxes):
+            for j, lens in enumerate(box):
+                n += (i + 1) * (j + 1) * lens.focal
+        return n
+
+    def split(self, line):
+        number = 0
+        symbol = "-"
+        if "-" in line:
+            tag = line[:-1]
+        else:
+            tag, number = line.split("=")
+            symbol = "="
+
+        return hash(tag), tag, symbol, int(number)
+
+    def hashmap(self):
+        for i in self.data:
+            boxn, tag, symbol, n = self.split(i)
+            lens = Lens(tag, n)
+            box = self.boxes[boxn]
+            if symbol == "-":
+                # remove the tag lens from the box, and shift the rest
+                if lens in box:
+                    box.remove(lens)
+            else:
+                # if there's lens, replace old lens with new
+                try:
+                    index = box.index(lens)
+                    box[index] = lens
+                except ValueError:
+                    # if no lens, add at the end
+                    box.append(lens)
+
+    def focusing_power(self, lens):
+        boxn = hash(lens.tag)
+        box = self.boxes[boxn]
+        pos = box.index(lens) + 1
+        return (boxn + 1) * pos * lens.focal
 
     def parseinput(self, lines):
         lines = super().parseinput(lines)
         data = lines[0].strip()
         data = data.split(",")
+        self.boxes = [list() for i in range(256)]
         return data
 
 
 if __name__ == "__main__":
-    print("Day 15: ")
+    print("Day 15: Lens Library")
 
     p = D15("d15.data")
     print(p.solve_p1())
