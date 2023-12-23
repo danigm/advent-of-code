0b7b1df9bab3930fcd294a4b5c2551ac7fe8b87e
Daniel García Moreno <dani@danigm.net>
2023-12-23 15:57:28 +0100

    2023 day 21, improve the search avoiding repetition
    Just calculate the longest path and add the explored that are possible
    in the same parity step.

--- 2023/d21.py
+++ 2023/d21.py
@@ -23,49 +23,77 @@ def test_part1():
 
 def test_part2():
     p = D21(example)
-    assert p.solve_p2() == 0
+    assert p.solve_p2(6) == 16
+    assert p.solve_p2(7) == 22
+    assert p.solve_p2(10) == 50
+    assert p.solve_p2(50) == 1594
+    assert p.solve_p2(100) == 6536
+    assert p.solve_p2(500) == 167004
 
 
 class D21(Problem):
     def solve_p1(self, steps=64):
         return self.count_reach(steps)
 
-    def solve_p2(self):
-        return 0
+    def solve_p2(self, steps=26501365):
+        return self.count_reach(steps, True)
 
-    def count_reach(self, steps):
+    def count_reach(self, steps, infinite=False):
         reach = set()
         reach.add(self.start)
+        explored = {}
+        initial_steps = steps
 
+        i = 0
         while steps:
             steps -= 1
+            i += 1
+
             nr = set()
             for r, c in reach:
-                nr = nr | self.possible_pots(r, c)
-            reach = nr
+                explored[(r, c)] = i
+                possible = self.possible_pots(r, c, infinite)
+                nr = nr | possible
+            reach = nr - set(i for i in explored)
 
-        return len(reach)
+        if initial_steps % 2:
+            x = set(i for i in explored if explored[i] % 2 == 0)
+        else:
+            x = set(i for i in explored if explored[i] % 2)
 
-    def possible_pots(self, r, c):
+        return len(reach) + len(x)
+
+    def inf_point(self, r, c):
+        return r % self.rows, c % self.cols
+
+    def possible_pots(self, r, c, infinite=False):
         pos = set()
         data = self.data
 
         # top
         nr, nc = r - 1, c
+        if infinite:
+            nr, nc = self.inf_point(nr, nc)
         if nr >= 0 and data[nr][nc] == ".":
-            pos.add((nr, nc))
+            pos.add((r - 1, c))
         # bottom
         nr, nc = r + 1, c
+        if infinite:
+            nr, nc = self.inf_point(nr, nc)
         if nr < self.rows and data[nr][nc] == ".":
-            pos.add((nr, nc))
+            pos.add((r + 1, c))
         # left
         nr, nc = r, c - 1
+        if infinite:
+            nr, nc = self.inf_point(nr, nc)
         if nc >= 0 and data[nr][nc] == ".":
-            pos.add((nr, nc))
+            pos.add((r, c - 1))
         # right
         nr, nc = r, c + 1
+        if infinite:
+            nr, nc = self.inf_point(nr, nc)
         if nc < self.cols and data[nr][nc] == ".":
-            pos.add((nr, nc))
+            pos.add((r, c + 1))
 
         return pos
 
